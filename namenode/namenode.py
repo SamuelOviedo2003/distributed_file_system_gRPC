da0dabc19eee237b30de986719348f2754f5212b
Samuel Oviedo Paz <oviedosamuel@gmail.com>
2024-09-07 17:56:25 -0500

    ls,mkdir, login, register

---
+++ namenode/namenode.py
@@ -0,0 +1,36 @@
+from concurrent import futures
+import grpc
+import proto.dfs_pb2 as pb2
+import proto.dfs_pb2_grpc as pb2_grpc
+from users import UserManager
+
+class NameNode(pb2_grpc.DFSServicer):
+    def __init__(self):
+        self.user_manager = UserManager()
+
+    def Login(self, request, context):
+        success, message = self.user_manager.authenticate(request.username, request.password)
+        return pb2.LoginResponse(success=success, message=message)
+
+    def CreateUser(self, request, context):
+        success, message = self.user_manager.create_user(request.username, request.password)
+        return pb2.CreateUserResponse(success=success, message=message)
+
+    def ListDirectories(self, request, context):
+        directories = self.user_manager.list_directories(request.username)
+        return pb2.ListResponse(directories=directories)
+
+    def MakeDirectory(self, request, context):
+        success, message = self.user_manager.make_directory(request.username, request.directory_name)
+        return pb2.MakeDirResponse(success=success, message=message)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_DFSServicer_to_server(NameNode(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
+
